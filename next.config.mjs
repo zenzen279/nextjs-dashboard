cf94896239b4af9eb8710d5464f292977ee12e99
Vincent Charpentier <vincent.charpentier7@gmail.com>
2024-06-29 12:13:04 +0200

    Fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    sassOptions : {
-    }
+    sassOptions: {
+    },
+    experimental: {
+        ppr: 'incremental',
+    },
 };
 
 export default nextConfig;
